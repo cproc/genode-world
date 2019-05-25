94e32137c8fcfaf52c1606d7f0e2a8012371b77e
Christian Prochaska <christian.prochaska@genode-labs.com>
2019-05-25 14:25:48 +0200

    ada_runtime_version.diff

--- lib/import/import-ada.mk
+++ lib/import/import-ada.mk
@@ -1,6 +1,6 @@
 ADA_RTS = $(BUILD_BASE_DIR)/var/libcache/ada
 
-ADA_RTS_SOURCE = $(call select_from_ports,ada-runtime)/ada-runtime/contrib/gcc-6.3.0
+ADA_RTS_SOURCE = $(call select_from_ports,ada-runtime)/ada-runtime/contrib/gcc-8.3.0
 ADA_RUNTIME_COMMON_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src/common
 ADA_RUNTIME_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src/fat
 ADA_RUNTIME_LIB_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src/lib
